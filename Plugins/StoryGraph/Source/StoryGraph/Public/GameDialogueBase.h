dfec1bb9da78e25452214f5512b60d602bb2b492
Unknown <c3148207@uon.edu.au>
2018-07-18 22:03:08 +1000

    Should be able to have the file in the build now

---
+++ Plugins/StoryGraph/Source/StoryGraph/Public/GameDialogueBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "GameDialogueBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(config = Engine, hidecategories = Object, abstract, editinnewline, BlueprintType)
+class STORYGRAPH_API UGameDialogueBase : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
